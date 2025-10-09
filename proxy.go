cea0342d53f9fb46849c946792448009c076a86e
sodiqscript111 <simidusodiqt@gmail.com>
2025-10-09 14:11:37 +0100

    Clean Up

--- proxy.go
+++ proxy.go
@@ -9,7 +9,7 @@ import (
 func NewProxy(target string) *httputil.ReverseProxy {
 	targetURL, err := url.Parse(target)
 	if err != nil {
-		log.Fatalf("Invalid target URL %s: %v", target, err)
+		log.Fatalf("Invalid  target URL %s: %v", target, err)
 	}
 	return httputil.NewSingleHostReverseProxy(targetURL)
 }
